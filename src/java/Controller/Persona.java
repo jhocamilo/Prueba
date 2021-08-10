7b6e7701bfa37a7b5252a746169117167752716d
jhocamilo <johangiraldo@unicomfacauca.edu.co>
2021-08-10 14:50:27 -0500

    Upload

---
+++ src/java/Controller/Persona.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Camilo Giraldo
+ */
+@Entity
+@Table(name = "Persona")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Persona.findAll", query = "SELECT p FROM Persona p"),
+    @NamedQuery(name = "Persona.findByIdentificacion", query = "SELECT p FROM Persona p WHERE p.identificacion = :identificacion"),
+    @NamedQuery(name = "Persona.findByNombre", query = "SELECT p FROM Persona p WHERE p.nombre = :nombre"),
+    @NamedQuery(name = "Persona.findByApellido", query = "SELECT p FROM Persona p WHERE p.apellido = :apellido"),
+    @NamedQuery(name = "Persona.findByFechaDeNacimiento", query = "SELECT p FROM Persona p WHERE p.fechaDeNacimiento = :fechaDeNacimiento"),
+    @NamedQuery(name = "Persona.findBySexo", query = "SELECT p FROM Persona p WHERE p.sexo = :sexo"),
+    @NamedQuery(name = "Persona.findByGrupoSanguineo", query = "SELECT p FROM Persona p WHERE p.grupoSanguineo = :grupoSanguineo")})
+public class Persona implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "Identificacion")
+    private Long identificacion;
+    @Basic(optional = false)
+    @Column(name = "Nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @Column(name = "Apellido")
+    private String apellido;
+    @Basic(optional = false)
+    @Column(name = "FechaDeNacimiento")
+    @Temporal(TemporalType.DATE)
+    private Date fechaDeNacimiento;
+    @Basic(optional = false)
+    @Column(name = "Sexo")
+    private String sexo;
+    @Basic(optional = false)
+    @Column(name = "GrupoSanguineo")
+    private String grupoSanguineo;
+
+    public Persona() {
+    }
+
+    public Persona(Long identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public Persona(Long identificacion, String nombre, String apellido, Date fechaDeNacimiento, String sexo, String grupoSanguineo) {
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.sexo = sexo;
+        this.grupoSanguineo = grupoSanguineo;
+    }
+
+    public Long getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(Long identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(Date fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getGrupoSanguineo() {
+        return grupoSanguineo;
+    }
+
+    public void setGrupoSanguineo(String grupoSanguineo) {
+        this.grupoSanguineo = grupoSanguineo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (identificacion != null ? identificacion.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Persona)) {
+            return false;
+        }
+        Persona other = (Persona) object;
+        if ((this.identificacion == null && other.identificacion != null) || (this.identificacion != null && !this.identificacion.equals(other.identificacion))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Controller.Persona[ identificacion=" + identificacion + " ]";
+    }
+    
+}
